f74cc74b7e0d1a658fa291761ae69eba97551ccc
Tannu Dhankhar <tannudhankhar81@gmail.com>
2024-08-01 00:33:38 +0530

    Update music_store_data_analysis.sql

--- music_store_data_analysis.sql
+++ music_store_data_analysis.sql
@@ -17,7 +17,8 @@ ORDER BY c DESC
 SELECT total 
 FROM invoice
 ORDER BY total DESC
-
+limit 3
+	
 /* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
 Write a query that returns one city that has the highest sum of invoice totals. 
 Return both the city name & sum of all invoice totals */
